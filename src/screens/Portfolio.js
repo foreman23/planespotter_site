c53b3ce27a6d6584215cc28f3d7acd7b7c3f8d06
foreman23 <foremanb@oregonstate.edu>
2023-04-12 23:39:46 -0700

    fixed portfolio mobile display

--- src/screens/Portfolio.js
+++ src/screens/Portfolio.js
@@ -1,4 +1,4 @@
-import { Image, Button, Loader, Icon, List, Menu, Dropdown, Divider } from "semantic-ui-react";
+import { Image, Button, Loader, Icon, List, Menu, Dropdown } from "semantic-ui-react";
 import { Container, Row, Col } from 'react-bootstrap';
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 import { useState } from "react";
@@ -146,28 +146,15 @@ const images = [
     // ------------------------------------------------------
 ];
 
-const tagOptions = [
-    {
-        key: '1',
-        text: '3',
-        value: 3,
-    },
-    {
-        key: '2',
-        text: '6',
-        value: 6,
-    },
-    {
-        key: '3',
-        text: '9',
-        value: 9,
-    },
-    {
-        key: '4',
-        text: '12',
-        value: 12,
-    },
-]
+const tagOptions = []
+
+for (let i = 3; i <= 24; i+=3) {
+    tagOptions.push({
+        key: i.toString(),
+        text: i.toString(),
+        value : i,
+    })
+}
 
 const aircraftOptions = [
     {
@@ -197,7 +184,7 @@ const Portfolio = (props) => {
 
     // Handles displaying images
     const [startIndex, setStartIndex] = useState(0);
-    const [imagesPerPage, setImagesPerPage] = useState(12);
+    const [imagesPerPage, setImagesPerPage] = useState(24);
 
     // go forward (imagesPerPage)
     const handleClickNext = () => {
@@ -254,36 +241,41 @@ const Portfolio = (props) => {
             <Container>
                 <Row>
                     <div className='portfolioBar'>
-                        <h5 style={{marginRight: '10px'}}>Page: {Math.ceil((startIndex + 1) / imagesPerPage)} of {Math.ceil(images.length / imagesPerPage)}</h5>
+                        <h5 style={{ marginRight: '10px' }}>Page: {Math.ceil((startIndex + 1) / imagesPerPage)} of {Math.ceil(images.length / imagesPerPage)}</h5>
                         <span>
-                            <Dropdown 
-                            placeholder="Aircraft"
-                            multiple
-                            search
-                            selection
-                            fluid
-                            style={{width: '30vw', marginRight: '10px'}}
-                            options={aircraftOptions}
+                            <Dropdown
+                                placeholder="Aircraft"
+                                multiple
+                                search
+                                selection
+                                fluid
+                                style={{ width: '30vw', marginRight: '10px' }}
+                                options={aircraftOptions}
                             >
                             </Dropdown>
                         </span>
 
+
+                        <span className="displayPerPage">Per page:</span>
                         <Menu compact>
                             <Dropdown onChange={handleFilterAmount} style={{ backgroundColor: '#e6e6e6' }} text={imagesPerPage} options={tagOptions} simple item />
                         </Menu>
 
                     </div>
-                <Divider></Divider>
                 </Row>
                 <Loader active={isLoading} inline='centered' size="small">Loading</Loader>
                 <span className="gallery">
                     <Row style={{ marginTop: '10px' }}>
                         <Col className="porfolioButtons" xs={1}>
-                            <Button onClick={handleClickPrev}><Icon name="chevron left"></Icon></Button>
+                            <Icon size="big" className="chevronButton" onClick={handleClickPrev} name="chevron left"></Icon>
                         </Col>
+                        <span className="hideDesktop mobilePortfolioButtons">
+                            <Button onClick={handleClickPrev}><Icon name="chevron left"></Icon></Button>
+                            <Button onClick={handleClickNext}><Icon name="chevron right"></Icon></Button>
+                        </span>
                         <Col>
                             <ResponsiveMasonry>
-                                <Masonry gutter="10px">
+                                <Masonry gutter="3px">
                                     {displayedImages.map((image) => (
                                         <Image className="galleryImage" onLoad={handleImageLoad} as='a' href="/portfolio" key={image.id} src={image.filename} />
                                     ))}
@@ -291,13 +283,13 @@ const Portfolio = (props) => {
                             </ResponsiveMasonry>
                         </Col>
                         <Col className="porfolioButtons" xs={1}>
-                            <Button onClick={handleClickNext}><Icon name="chevron right"></Icon></Button>
+                            <Icon size="big" className="chevronButton" onClick={handleClickNext} name="chevron right"></Icon>
                         </Col>
                     </Row>
-                    <Row>
-                        <Col className="hideDesktop portfolioButtonsMobile"><Button onClick={handleClickPrev}><Icon name="chevron left"></Icon></Button></Col>
-                        <Col className="hideDesktop portfolioButtonsMobile"><Button onClick={handleClickNext}><Icon name="chevron right"></Icon></Button></Col>
-                    </Row>
+                    <span className="hideDesktop mobilePortfolioButtons">
+                        <Button onClick={handleClickPrev}><Icon name="chevron left"></Icon></Button>
+                        <Button onClick={handleClickNext}><Icon name="chevron right"></Icon></Button>
+                    </span>
                     <Row style={{ marginTop: '10px' }}>
                         <List className='portfolioPageNums' horizontal>
                             {pageNums.map((index) => (
